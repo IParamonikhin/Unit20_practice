347eb63b96be4ca1f619d4eed9cf5f280deefecf
Ivan <paramonikhin.ivan@yandex.ru>
2023-05-14 21:02:17 +0300

    Initial Commit

---
+++ Unit20_practice/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Unit20_practice
+//
+//  Created by Иван on 14.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
